dce8027918bfda65e04da4dc105e4c7112de041d
Pierce Wang <piercewang@pierces-air.lan>
2022-07-05 11:11:36 -0700

    Working notes generation based on dates

--- src/NotesEntryGenerator.R
+++ src/NotesEntryGenerator.R
@@ -1,16 +1,6 @@
 #!/usr/bin/env Rscript
 args = commandArgs(trailingOnly=TRUE)
-## EDIT CODE BELOW THIS LINE
-
-## Each item in "list_to_generate" is a list consisting of:
-## 1. vector containing ALL of a single patient's IDs written in all lowercase. Format: c("gest24", "vist34")
-## 2. string of patient's name to display as report title. Format: "Emily Jusuf"
-## 3. (optional) date of earliest survey and tech data to include. Format: "yyyy-mm-dd"
-
-## Setting "rounds" to TRUE will display a patient's demographics, session number, and planned course
-## at the top of the report.
-## Only do this if "Demographics.csv" AND session numbers in "Tech.csv" are fully updated!
-rounds <- TRUE
+error_file <-"../output/error_log.txt"
 
 pkgLoad <- function( packages = "std" ) {
 
@@ -41,67 +31,96 @@ pkgLoad <- function( packages = "std" ) {
             library( package, character.only = TRUE, quietly = TRUE )
         )
     }
-
 }
 
 ## Ensure all packages are loaded/installed.
 pkgLoad()
-options(gargle_oauth_email = "pierce.g.wang@gmail.com")
+options(gargle_oauth_email = "***REMOVED***")
 ## gs4_auth(gargle_oauth_email = "pierce.g.wang@gmail.com")
 
-survey_raw <- read_sheet("1AFGLSMBQyUNUL0p84mZ-xusaHbLrAveUp-uGTxWl3r4", "Sheet2") ## >% select(-"Txt Sum", -"Email", -"Name", -contains(".of.16."),
-##                                                 -contains(" of 21 "), -contains(" of 9 "), -contains(" if none "),
-##                                                 -starts_with("Have "), -starts_with("If ")) %>%
-##   mutate(Date = as.Date("Completion time", "%m/%d/%y"),
-##          Patient = tolower(Patient)) %>%
-##   rename(SurveyID = "What survey(s) would you like to complete?",
-##          GAD7 = GAD7_)
-## print(args[1])
 numbers_only <- function(x) !grepl("\\D", x)
-for (id in args) {
-    if (!numbers_only(id))
-        printf("%s not valid ID", id)
-    else {
-        survey_entry <- survey_raw[survey_raw$ID == id,]
-        print(class(survey_entry)) ## Debug
-        output_file <- sprintf("../output/%s.txt", id)
-cat(sprintf("Date: %s
-Stimulation Parameters:
-Machine: MagVenture MagPro R30 with Cool-B65 coil.
-xx total sessions each separated by at least 45 minutes.
 
-Session xx:  xx:xx
-
+generate_output <- function(date) {
+    survey_entries <- tech_raw[as.Date(tech_raw$Timestamp) == date,]
+    if (!(nrow(survey_entries) == 0)) {
+    ## print(class(survey_entry)) ## Debug
+    output_file <- sprintf("../output/%s.html", as.Date(date, origin="1970-01-01"))
+    file.create(output_file)
+    cat(sprintf("<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset=\"UTF-8\">
+    <title>Entries for Date: %s</title>
+  </head>
+  <body>\n", as.Date(date, origin="1970-01-01")), file=output_file, fill=FALSE)
+    notes_template <- "\n<hr>\n\nDate: %s <br />
+Stimulation Parameters:<br />
+Machine: MagVenture MagPro R30 with Cool-B65 coil.<br />
+xx total sessions each separated by at least 45 minutes.<br />
+<br />
+Session xx:  xx:xx<br />
+<br />
 %s
-
-Protocol A: %s stimulated with %s at %i%% MT (%i%% MO)
-%s
-Technician Notes: %s
-Technician: %s
-Supervision: Supervised by %s. %s.
-
-Course:  Session # %s of %s planned.
-/////-----Patient Mood Surveys-----/////
-",
-survey_entry$'Start time',
-if(survey_entry$'Did you re-threshold since the last session?'=="Yes") sprintf("Motor Threshold today was measured to be %s%% MO. It was performed by %s due to %s.",
-                                                                               survey_entry$'What is the new motor threshold?',
-                                                                               survey_entry$'What is the full name of the person that performed the re-threshold?',
-                                                                               survey_entry$'Why did you re-threshold?') else "",
-survey_entry$'What is the first target?',
-survey_entry$'What is the first protocol?',
-survey_entry$'What is the machine output as a percent of the motor threshold for the first target?' * 100,
-survey_entry$'What did you set the machine output for the first target?',
-if(survey_entry$'Is there a second target?' == "Yes") sprintf("Protocol B: %s stimulated with %s at %i%% MT (%i%% MO)",
-                                                              survey_entry$'What is the second target?',
-                                                              survey_entry$'What is the second protocol?',
-                                                              survey_entry$'What is the amchine output as a percent of hte motor threshold for the second target?' * 100,
-                                                              survey_entry$'What did you set the machine output for the second target?') else "",
-survey_entry$'Please write your notes for this patient (select all that may apply)',
-survey_entry$'Technician\'s Full Name',
-survey_entry$'Who is the supervising psychiatrist?',
-survey_entry$'What is the supervising level?',
-survey_entry$'Tx',
-survey_entry$'Planned Course'), file=output_file,fill=FALSE)
-        }
+<br />
+Protocol A: %s stimulated with %s at %s%% MT (%s%% MO)<br />
+%s<br />
+Technician Notes: %s<br />
+Technician: %s<br />
+Supervision: Supervised by %s. %s.<br />
+<br />
+Course:  Session # %s of %s planned.<br />
+/////-----Patient Mood Surveys-----/////\n"
+    for (i in 1:nrow(survey_entries)) {
+      entry <- survey_entries[i,]
+      output <- sprintf(notes_template, entry$Timestamp,
+                        if(entry$'Did you re-threshold since the last session?'=="Yes") sprintf("Motor Threshold today was measured to be %s%% MO. It was performed by %s due to %s.<br />",
+                                                                                 entry$'What is the new motor threshold?',
+                                                                                 entry$'What is the full name of the person that performed the re-threshold?',
+                                                                                 entry$'Why did you re-threshold?') else "",
+                        entry$'What is the first target?',
+                        entry$'What is the first protocol?',
+                        as.numeric(entry$'What is the machine output as a percent of the motor threshold for the first target?') * 100,
+                        entry$'What did you set the machine output for the first target?',
+                        if(entry$'Is there a second target?' == "Yes") sprintf("Protocol B: %s stimulated with %s at %s%% MT (%s%% MO)<br />",
+                                                                                      entry$'What is the second target?',
+                                                                                      entry$'What is the second protocol?',
+                                                                                      as.numeric(entry$'What is the machine output as a percent of the motor threshold for the second target?') * 100,
+                                                                                      entry$'What did you set the machine output for the second target?') else "",
+                        entry$'Please write your notes for this patient (select all that may apply)',
+                        ## survey_entry$'Technician\'s Full Name',
+                        entry$'Technician\'s Full Name', ## one or the other (above); TODO: Standardize column names
+                        entry$'Who is the supervising psychiatrist?',
+                        entry$'What is the supervising level?',
+                        entry$'Treatment #',
+                        entry$'Planned Course')
+          if (output != "")
+              cat(output, file=output_file, fill=FALSE, append=TRUE)
+          else
+              cat(sprintf("Error in entry ID, %i\n", id), file=error_file, fill=FALSE, append=TRUE)
+    }
+    cat("</body>
+</html>", file=output_file, fill=FALSE, append=TRUE)
+    }
 }
+
+
+cat(sprintf("Error log for %s:\n", Sys.time()), file=error_file, fill=FALSE, append=FALSE)
+
+tech_raw <- read_sheet("***REMOVED***", "Query")
+
+for (date in args) {
+    if (grepl("-", date) && !is.na(as.Date(date, "%m/%d/%y")) && !is.na(as.Date(str_extract(date, "[/0-9]+$"), "%m/%d/%y"))) {
+        print(sprintf("Output range of dates: %s", date))
+        range_bounds <- as.Date(str_split(date, "-")[[1]], "%m/%d/%y")
+        for (i in range_bounds[1]:range_bounds[2]) {
+            generate_output(i)
+        }
+    } else if (is.na(as.Date(date, "%m/%d/%y"))) {
+        print("%s not valid ID", id)
+    } else {
+        print(sprintf("Output notes for entry date %s", date))
+        generate_output(as.Date(date, "%m/%d/%y"))
+    }
+}
+
+cat("End of error log.", file=error_file, fill=FALSE, append=TRUE)
